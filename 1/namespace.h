f9f74aa551b322f69e5805c9615c5565d651cd5b
sardpade <sunwh1004184@163.com>
2020-03-14 11:27:18 +0800

    1.add  namspace

---
+++ 1/namespace.h
@@ -0,0 +1,28 @@
+std::cout<<2.0<<std::endl;
+
+using std::cout;
+using std::endl;
+cout<<2.0<<endl;
+
+
+using namespace std;
+cout<<2.0<<endl;
+
+
+
+
+//--------------------------------------
+#include <iostream>
+#include <stdio.h>
+
+
+using namespace std;
+void main()
+{
+	cout<<"欢迎您开始步入C++标准模板库"<<endl;
+	getchar();
+	return;	
+
+}
+
+//--------------------------------------
